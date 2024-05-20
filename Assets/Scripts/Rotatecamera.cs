b06f8e9cbcdc1526a0fd7ecf64d6e0bfbeb38763
MettoNao <nt7.00.harry@gmail.com>
2024-05-21 01:39:09 +0900

    グラフィック追加

--- Assets/Scripts/Rotatecamera.cs
+++ Assets/Scripts/Rotatecamera.cs
@@ -11,71 +11,41 @@ public class Rotatecamera : MonoBehaviour
     [SerializeField] private float MaxAngle;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        GravityScript gravityScript;
-        GameObject obj = GameObject.Find("Player"); //Playerっていうオブジェクトを探す
-        gravityScript = obj.GetComponent<GravityScript>(); //付いているスクリプトを取得
         SetAngle();
-        if (gravityScript.gravityL)
-        {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(-90f, Vector3.forward)*GetAngle , 5f);
-        }
-        else if (gravityScript.gravityR)
-        {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(90f, Vector3.forward) * GetAngle, 5f);
-        }
-        else if (gravityScript.gravityZP)
-        {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(-90f, Vector3.right) * GetAngle, 5f);
-        }
-        else if (gravityScript.gravityZM)
-        {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(90f, Vector3.right) * GetAngle, 5f) ;
-        }
-        else if (gravityScript.gravityUP)
-        {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(180f, Vector3.forward) * GetAngle, 5f);
-        }
-        else
-        {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(0, Vector3.forward) * GetAngle, 5f);
-        }
+
+        Quaternion targetRotation = Quaternion.FromToRotation(transform.up, -Physics.gravity) * transform.rotation;
+        Quaternion additionalRotation = Quaternion.Euler(0, XAngle, 0);
+        Quaternion finalTargetRotation = targetRotation * additionalRotation;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, finalTargetRotation, 5);
+        transform.Rotate(-Physics.gravity, XAngle * Time.deltaTime);
     }
 
     private void SetAngle()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            XAngle += AngleSpeed * Time.deltaTime;
-            if(XAngle >=MaxAngle)
-              XAngle = MaxAngle; 
+            XAngle = AngleSpeed;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            XAngle = 0;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            XAngle -= AngleSpeed * Time.deltaTime;
-            if (XAngle <= -MaxAngle)
-                XAngle = -MaxAngle;
+            XAngle = -AngleSpeed;
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKeyUp(KeyCode.RightArrow))
         {
-            YAngle += AngleSpeed * Time.deltaTime;
-            if (YAngle >= MaxAngle)
-                YAngle = MaxAngle;
+            XAngle = 0;
         }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            YAngle -= AngleSpeed * Time.deltaTime;
-            if (YAngle <= -MaxAngle)
-                YAngle = -MaxAngle;
-        }
-
     }
 
-    private Quaternion GetAngle => Quaternion.AngleAxis(XAngle, Vector3.up)* Quaternion.AngleAxis(YAngle, Vector3.right);
+    private Quaternion GetAngle => Quaternion.AngleAxis(YAngle, transform.right);
 
 }
